a8a4f2cf29ad5fa57587b920dc53819105c8ff40
Alexsey at Home <send.popov@mail.ru>
2022-07-11 00:44:27 +0300

    Feat. Add Bootstrap 5 and JQuery, Build in Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,26 @@
 import './bootstrap';
+import * as bootstrap from 'bootstrap'
 
+import * as jqueryExports from "jquery";
+window.$ = jqueryExports.default;
+
+// Импортируем стили
 import '../sass/app.scss'
 
-import * as bootstrap from 'bootstrap'
+// Модальник для удаления
+const deleteModal = document.getElementById('deleteModal')
+deleteModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const name = button.getAttribute('data-bs-name')
+    const link = button.getAttribute('data-bs-link')
+
+    const modalTitle = deleteModal.querySelector('.modal-title')
+    const modalText = deleteModal.querySelector('.modal-text')
+    const modalButton = deleteModal.querySelector('.modal-button')
+
+    modalTitle.textContent = `Подтверждение удаления ${name}`
+    modalText.textContent = `Вы уверены, что хотите удалить ${name}?`
+    modalButton.href = link
+})
