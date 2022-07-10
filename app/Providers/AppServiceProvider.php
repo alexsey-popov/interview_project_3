f71494c8a49b5df3a4561c423c7c02090cf97c5b
Alexsey at Home <send.popov@mail.ru>
2022-07-11 00:42:27 +0300

    Feat. Add support MariaDB system versioned

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Решение для работы с Mariadb system versioned tables
+        // https://stackoverflow.com/questions/66933669/how-to-write-eloquent-query-for-mariadb-system-versioning
+        Builder::macro("bySystemTime", function(string $timestamp = null) {
+            $timestamp = $timestamp ? "AS OF TIMESTAMP '$timestamp'" : 'ALL';
+            $this->from = DB::raw("`$this->from` FOR SYSTEM_TIME $timestamp");
+            return $this;
+        });
     }
 }
