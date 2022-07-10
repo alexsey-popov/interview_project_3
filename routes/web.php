61ff8cf6b2c59ff99ede92e214c481bdfa388b87
Alexsey at Home <send.popov@mail.ru>
2022-07-11 00:44:49 +0300

    Feat. Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PriceListController;
+use App\Http\Controllers\PriceListItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PriceListController::class)->group(function () {
+    Route::get('/', 'index')->name('index');
+    Route::get('/price-list/{id}', 'show')->whereNumber('id')->name('price-list-show');
+    Route::get('/price-list/{id}/edit', 'edit')->whereNumber('id')->name('price-list-edit');
+    Route::post('/price-list/{id}/edit', 'update')->whereNumber('id');
+    Route::get('/price-list/{id}/delete', 'destroy')->whereNumber('id');
+
+    Route::get('/export', 'export')->name('export');
+    Route::get('/download', 'download')->name('download');
+});
+
+Route::controller(PriceListItemController::class)->group(function () {
+    Route::get('/price-list/{list}/{item}/edit', 'edit')->whereNumber('list', 'item')->name('price-list-item-edit');
+    Route::post('/price-list/{list}/{item}/edit', 'update')->whereNumber('list', 'item');
+    Route::get('/price-list/{list}/{item}/delete', 'destroy')->whereNumber('list', 'item');
 });
